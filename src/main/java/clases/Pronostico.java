93722e5d84591f2b3449dc7c891af68b9c4f5edf
Manuel <trapecioinv@gmail.com>
2023-04-22 16:41:41 -0300

    Logica para obtener pronosticos con nombre de jugador

--- src/main/java/clases/Pronostico.java
+++ src/main/java/clases/Pronostico.java
@@ -8,16 +8,20 @@ import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Pronostico {
     private Partido partido;
     private Equipo equipo;
     private ResultadoEnum resultado;
+    private String nombreJugador;
 
-    public Pronostico(Partido partido, Equipo equipo, ResultadoEnum resultado) {
+    public Pronostico(Partido partido, Equipo equipo, ResultadoEnum resultado, String nombreJugador) {
         this.partido = partido;
         this.equipo = equipo;
         this.resultado = resultado;
+        this.nombreJugador = nombreJugador;
     }
 
     public int puntos() throws IOException {
@@ -33,16 +37,34 @@ public class Pronostico {
     public static List<Pronostico> buildListPronostico(File pronosticoFile, List<Partido> listPartidos) throws IOException {
         List<String> pronosticoLines = Files.readAllLines(pronosticoFile.toPath());
 
-        // Armamos una list que contenga las X y los espacios Vacios
+        // Armamos una list que contenga las X y los espacios Vacios-
+        // Tambien armamos una lista para los nombres de los jugadores.
         List<String> listPosicionX = new ArrayList<>();
+        List<String> listNombreJugador = new ArrayList<>();
+        List<String> nombresDeEquipo = new ArrayList<>();
+
+        // Obtenemos todos los nombres de equipo para diferenciar
+        if(Objects.nonNull(listPartidos) && !listPartidos.isEmpty()) {
+             nombresDeEquipo = listPartidos.stream()
+                    .flatMap(partido -> Stream.of(partido.getEquipo1().getNombre(), partido.getEquipo2().getNombre()))
+                    .collect(Collectors.toList());
+        } else {
+            throw new IOException("La lista de partidos esta vacio o es nula.");
+        }
+
 
         for(String pronosticoLine : pronosticoLines) {
             // Separamos los datos con coma por cada linea.
-            String[] resultadoSplit = pronosticoLine.split(",");
+            String[] resultadoSplit = pronosticoLine.split(";");
             // Obtenemos los datos a partir del array "spliteado"
             for(String splitDato : resultadoSplit) {
                 if(splitDato.equalsIgnoreCase("X") || splitDato.isEmpty()) {
                     listPosicionX.add(splitDato);
+                } else {
+                    // Si el dato encontrado no es un nombre de equipo, se agrega a la lista de jugadores.
+                    if(!nombresDeEquipo.contains(splitDato)) {
+                        listNombreJugador.add(splitDato);
+                    }
                 }
             }
 
@@ -50,8 +72,8 @@ public class Pronostico {
 
         // Creamos la lista de pronosticos
         List<Pronostico> listPronosticos = new ArrayList<>();
-
-            for(int i = 0; i < listPosicionX.size(); i = i) {
+        int i = 0;
+            while(i < listPosicionX.size()) {
                 // Obtenemos lo que haya en la lista, ya sea X o Espacio y evaluamos.
                 String pronostico = listPosicionX.get(i);
 
@@ -82,8 +104,18 @@ public class Pronostico {
                         break;
                     }
 
-                    // Similar al caso de la posicionX, utilizamos obtenerIndicePartidoPorPosX para obtener el partido correcto.
-                    Partido partido = listPartidos.get(Pronostico.obtenerIndicePartidoPorPosX(i));
+                    // Similar al caso de la posicionX, utilizamos obtenerIndicePartidoPorPosX para obtener el partido y nombre de jugador correcto.
+                    int indicePartidos = Pronostico.obtenerIndicePartidoPorPosX(i);
+                    int indiceNombre = Pronostico.obtenerIndicePartidoPorPosX(i);
+
+                    // Regresamos al principio de la lista de partidos siempre que todavia haya jugadores disponibles.
+                    if (indicePartidos >= listPartidos.size() && indicePartidos < listNombreJugador.size()) {
+                        indicePartidos -= listPartidos.size();
+                    }
+
+                    Partido partido = listPartidos.get(indicePartidos);
+                    String nombreJugador = listNombreJugador.get(indiceNombre);
+
 
                     if(!empate) {
                         if(gana1) {
@@ -97,7 +129,7 @@ public class Pronostico {
                         resultado = ResultadoEnum.EMPATE;
                     }
 
-                    Pronostico pronosticoResult = new Pronostico(partido, equipoSeleccionado, resultado);
+                    Pronostico pronosticoResult = new Pronostico(partido, equipoSeleccionado, resultado, nombreJugador);
 
                     if(Objects.nonNull(pronosticoResult)){
                         listPronosticos.add(pronosticoResult);
@@ -116,7 +148,7 @@ public class Pronostico {
     public static int obtenerIndicePartidoPorPosX(int posicionX) {
         // 3 posiciones de X == 1 partido. Vamos restando de a 3 posiciones para encontrar el lugar de cada partido.
         int indicePartido = 0;
-        while (posicionX - 3 > indicePartido) {
+        while (posicionX - 3 >= indicePartido) {
             posicionX -= 3;
             indicePartido++;
         }
@@ -125,7 +157,7 @@ public class Pronostico {
 
     public static int obtenerPosXPorIndice(int indice) {
         // Restamos de a 3 valores para obtener siempre 0, 1 o 2.
-        while(indice > 3) {
+        while(indice >= 3) {
             indice -= 3;
         }
         return indice;
@@ -156,11 +188,20 @@ public class Pronostico {
         this.resultado = resultado;
     }
 
+    public String getNombreJugador() {
+        return nombreJugador;
+    }
+
+    public void setNombreJugador(String nombreJugador) {
+        this.nombreJugador = nombreJugador;
+    }
+
     @Override
     public String toString() {
         return "Pronostico{" +
                 "equipo=" + this.getEquipo().toString() +
                 ", resultado=" + this.getResultado().getValue() +
+                ", nombreJugador=" + this.getNombreJugador() +
                 ", partido=" + this.getPartido().toString() +
                 '}';
     }
