ce2bd14a510f23db8b37a31881812d9e5c6d2979
Manuel <trapecioinv@gmail.com>
2023-04-22 17:03:13 -0300

    Sistema para obtener los puntajes de distintos jugadores. TODO: Sistema de rondas

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -32,18 +33,38 @@ public class Main {
 
         List<Pronostico> listPronostico = Pronostico.buildListPronostico(pronosticoFile, listPartidos);
 
+        // Armamos un set para no tener repetidos los nombres.
+        Set<String> setNombres = new HashSet<>(listPronostico.stream()
+                .map(pronostico -> pronostico.getNombreJugador())
+                .collect(Collectors.toList()));
+
         // Imprimimos por pantalla el valor de las instancias del objeto Partido.
+        System.out.println("========== PARTIDOS =========");
         for(Partido p : listPartidos) {
             System.out.println(p.toString());
         }
 
-        int puntaje = 0;
-        // Imprimimos por pantalla el valor de las instancias del objeto Pronostico.
+        System.out.println("============ PRONOSTICOS ==========");
+        // Imprimimos por pantalla el valor de las instancias del objeto Partido.
         for(Pronostico pro : listPronostico) {
             System.out.println(pro.toString());
-            puntaje += pro.puntos();
         }
 
-        System.out.println("============ EL PUNTAJE FINAL ES ->>> " + puntaje + " <<<- ============");
+        System.out.println("============ LOS PUNTAJES SON: ============");
+        for(String nombre : setNombres) {
+            int puntajeJugador = listPronostico.stream()
+                    .filter(pronostico -> pronostico.getNombreJugador().equals(nombre)) // Obtenemos los pronosticos que correspondan al nombre del jugador
+                    .map(pro -> { // Obtneemos el puntaje del jugador
+                        int puntaje = 0;
+                        try {
+                            puntaje += pro.puntos();
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                        return puntaje;
+                    }).reduce(0, (x,y) -> x+y); // Usamos reduce para sumar el puntaje total
+
+            System.out.println(nombre + ": " + puntajeJugador); // Imprimimos el nombre y puntaje por consola.
+        }
     }
 }
