9109e04f6420b9d3a93e92dbb7092cf4c36c816e
Gustavo <gustavotobiasp@gmail.com>
2023-04-24 11:23:45 -0300

    puntajes

--- src/test/java/PuntajesTest.java
+++ src/test/java/PuntajesTest.java
@@ -2,6 +2,7 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.sql.SQLException;
 import java.util.List;
 
 import org.junit.jupiter.api.Assertions;
@@ -20,47 +21,47 @@ public class PuntajesTest {
 	private List<Puntajes> pts, pts2;
 	
 	@BeforeEach
-	public void setup() throws IOException {
+	public void setup() throws IOException, SQLException {
 		Path arch= Paths.get(new File ("src\\test\\resources\\PartidosTest.csv").getAbsolutePath()); 
-		Path arch2=Paths.get(new File ("src\\test\\resources\\PronosticoTest.csv").getAbsolutePath());
+		String arch2="src\\test\\resources\\PronosticoTest.csv";
 		
 		
 		resulPartidos = Partido.buildListPartidosFromFile(new File (arch.toUri()));
-		resulPronosticos = Pronostico.buildListPronostico(new File (arch2.toUri()), resulPartidos);
+		resulPronosticos = Pronostico.buildListPronostico(arch2, resulPartidos);
 	}
 	
 	
-//	@DisplayName ("Puntaje de una persona en 2 rondas consecutivas")
-//	@Test 
-//	public void Test1() throws IOException {
-//		int esperado=7;		
-//		pts=Puntajes.Puntos(resulPartidos, resulPronosticos, 1, 1, 2);
-//		
-//		for(Puntajes puntos: pts) {
-//			Assertions.assertEquals(esperado,puntos.getPts());
-//		}
-//	}
-//	
-//	@DisplayName ("Nombre de persona correcto")
-//	@Test
-//	public void test2()throws IOException{
-//		String esperado="MARIANA";		
-//		pts=Puntajes.Puntos(resulPartidos, resulPronosticos, 1, 1, 2);
-//		
-//		for(Puntajes puntos: pts) {
-//			Assertions.assertEquals(esperado,puntos.getNombre());
-//		}
-//	}
+	@DisplayName ("Puntaje de una persona en 2 rondas consecutivas")
+	@Test 
+	public void Test1() throws IOException {
+		int esperado=7;		
+		pts=Puntajes.Puntos(resulPartidos, resulPronosticos, 1, 1, 2);
+		
+		for(Puntajes puntos: pts) {
+			Assertions.assertEquals(esperado,puntos.getPts());
+		}
+	}
+	
+	@DisplayName ("Nombre de persona correcto")
+	@Test
+	public void test2()throws IOException{
+		String esperado="MARIANA";		
+		pts=Puntajes.Puntos(resulPartidos, resulPronosticos, 1, 1, 2);
+		
+		for(Puntajes puntos: pts) {
+			Assertions.assertEquals(esperado,puntos.getNombre());
+		}
+	}
 	
 	@DisplayName ("Probando la suma del extra por fase")
 	@Test
-	public void test3()throws IOException{
-		int esperado=15;
+	public void test3()throws IOException, SQLException{
+		int esperado=16;
 		
 		Path arch3= Paths.get(new File ("src\\test\\resources\\PartidosTest2.csv").getAbsolutePath());
-		Path arch4=Paths.get(new File ("src\\test\\resources\\PronosticoTest2.csv").getAbsolutePath());
+		String arch4="src\\test\\resources\\PronosticoTest2.csv";
 		resulPartidos2 = Partido.buildListPartidosFromFile(new File (arch3.toUri()));
-		resulPronosticos2 = Pronostico.buildListPronostico(new File (arch4.toUri()), resulPartidos2);
+		resulPronosticos2 = Pronostico.buildListPronostico(arch4, resulPartidos2);
 		
 		pts2=Puntajes.Puntos(resulPartidos2, resulPronosticos2, 1, 1, 2);
 		
